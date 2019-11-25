b0bceaab4048837eddb0de0eeba31f07bef214e0
marnifora <marnifora@gmail.com>
2019-11-25 17:16:47 +0100

    Stars again! (python version changed)

--- train.py
+++ train.py
@@ -319,12 +319,10 @@ for epoch in range(num_epochs):
     logger.info("--{:>18s} :{:>5} seqs{:>15}".format('VALIDATION', val_len, "--"))
     for cl, seqs, sens, spec in zip(dataset.classes, data_labels[1], val_sens, val_spec):
         logger.info('{:>20} :{:>5} seqs - {:1.3f}, {:1.3f}'.format(cl, seqs, sens, spec))
-    train_means = list(map(mean, [train_sens, train_spec]))
-    valid_means = list(map(mean, [val_sens, val_spec]))
     logger.info(
-        "--{:>18s} : {:1.3f}, {:1.3f}{:>12}".format('TRAINING MEANS', train_means[0], train_means[1], "--"))
+        "--{:>18s} : {:1.3f}, {:1.3f}{:>12}".format('TRAINING MEANS', *list(map(mean, [train_sens, train_spec])), "--"))
     logger.info(
-        "--{:>18s} : {:1.3f}, {:1.3f}{:>12}\n\n".format('VALIDATION MEANS', valid_means[0], valid_means[1], "--"))
+        "--{:>18s} : {:1.3f}, {:1.3f}{:>12}\n\n".format('VALIDATION MEANS', *list(map(mean, [val_sens, val_spec])), "--"))
 
     if mean(val_sens) >= acc_threshold:
         logger.info('Validation accuracy threshold reached!')
