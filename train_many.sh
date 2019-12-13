103914794f40f27e92e07d59839a19dc07700f75
marnifora <marnifora@gmail.com>
2019-12-13 15:13:12 +0100

    Adjustments in parsing arguments in train_many.sh

--- train_many.sh
+++ train_many.sh
@@ -1,51 +1,41 @@
 #!/usr/bin/env bash
 
-usage="usage: $(basename "$0") [-h] [-p DIR] [--run NUM] NUMBER DATASET
+usage="usage: $(basename "$0") [-h] [--run NUM] NUMBER *PARAMS
 
 train many networks in a row
 
 positional arguments:
    NUMBER               number of networks to train
-   DATASET              name of the dataset which should be used for training
+   *PARAMS              parameters for the train.py script
 
 optional arguments:
    -h, --help           show this help message and exit
-   -p, --path           working directory
    --run                number from which run numbers should start, default: 1
    "
 
-if [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
-  echo "$usage"
-  exit
+if [ "$1" != '--help' ] | [ "$1" != '-h' ]; then
+  number=$1
+  shift
 fi
-
-number=$1
-dataset=$2
 run=1
-shift
-shift
+arguments=''
+
 while [ "$1" != "" ]; do
     case $1 in
-        -p | --path )           shift
-                                path=$1
-                                ;;
-        --run )                 shift
-                                run=$1
-                                ;;
-        -h | --help )           echo "$usage"
-                                exit
-                                ;;
-        * )                     echo "$usage"
-                                exit 1
+        --run )           shift
+                          run=$1 ;;
+        -h | --help )     echo "$usage"
+                          python3 train.py --help
+                          exit ;;
+        * )               arguments="$arguments $1"
     esac
     shift
 done
-
 
 j=1
 for i in $(seq "$run" 1 $(("$run"+"$number"-1)))
 do
   printf "\nTRAINING NETWORK %d / %d\n" "$j" "$number"
-  python3 train.py $dataset --path $path --run $i
+  python3 train.py "$arguments" --run $i
   j=$(("$j"+1))
 done
