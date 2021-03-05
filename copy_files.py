39f900811d02bad83992c5c3cd71e6fb5e7a53df
Piotr Lembryk <piotr.lembryk@gmail.com>
2021-03-05 08:42:57 +0100

    added script to copy results

--- copy_files.py
+++ copy_files.py
@@ -5,7 +5,7 @@ command1= 'mkdir MgrPtit/sRes/pamfl{x}'
 command2='cp MgrPtit/results/pamfl{x}/pamfl{x}_pamfl_params.csv MgrPtit/sRes/pamfl{x}'
 command3='cp MgrPtit/results/pamfl{x}/pamfl{x}_train_results.tsv MgrPtit/sRes/pamfl{x}'
 
-for i in range(10, 19):
+for i in range(19, 20):
     os.system(command1.format(x=i))
     os.system(command2.format(x=i))
     os.system(command3.format(x=i))
