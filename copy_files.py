52dd98c3a5a04cf7880d22bad02e5293ddc285f7
Piotr Lembryk <piotr.lembryk@gmail.com>
2021-03-05 08:47:50 +0100

    l

--- copy_files.py
+++ copy_files.py
@@ -5,7 +5,7 @@ command1= 'mkdir MgrPtit/sRes/pamfl{x}'
 command2='cp MgrPtit/results/pamfl{x}/pamfl{x}_pamfl_params.csv MgrPtit/sRes/pamfl{x}'
 command3='cp MgrPtit/results/pamfl{x}/pamfl{x}_train_results.tsv MgrPtit/sRes/pamfl{x}'
 
-for i in range(19, 20):
+for i in range(9, 10):
     os.system(command1.format(x=i))
     os.system(command2.format(x=i))
     os.system(command3.format(x=i))
